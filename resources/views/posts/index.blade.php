2ab5dcb45a9cfa1e8060d1ca82ee7f975b593901
Danijela Lapcevic <dannie@eunet.rs>
2015-05-25 17:55:11 +0200

    changing blade directives in view files, becose of escaping content

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,7 @@
 <article>
     <h2>{!! link_to_route('post', $post->title, array($post->id)) !!}</h2>
     <p class="created_at">Created on {{{ date('Y-m-d', strtotime($post->created_at))}}} By {{{ $post->user->name }}}
-    <p>{{{ str_limit($post->body) }}}</p>
+    <p>{!! str_limit($post->body) !!}</p>
     <p>{!! link_to_route('post', 'Read More &rsaquo;', array($post->id)) !!}</p>
 </article>
 @endforeach
